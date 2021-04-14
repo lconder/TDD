f5bbe9bad0e9e9c3cbb959462a2c160d4e6c763d
David Amaya <d.acruz@alumnos.upm.es>
2021-04-14 20:51:38 +0200

    Red

--- src/test/java/usantatecla/IntervalTest.java
+++ src/test/java/usantatecla/IntervalTest.java
@@ -1,88 +1,90 @@
 package usantatecla;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class IntervalTest {
 
-  private Point left = new Point(-2.2);
-  private Point right = new Point(4.4);
-  private IntervalBuilder intervalBuilder;
+    private Point left = new Point(-2.2);
+    private Point right = new Point(4.4);
+    private IntervalBuilder intervalBuilder;
 
-  @BeforeEach
-  public void before(){
-    this.left = new Point(-2.2);
-    this.right = new Point(4.4);
-    this.intervalBuilder = new IntervalBuilder();
-  }
+    @BeforeEach
+    public void before() {
+        this.left = new Point(-2.2);
+        this.right = new Point(4.4);
+        this.intervalBuilder = new IntervalBuilder();
+    }
 
-  @Test
-  public void givenIntervaOpenOpenlwhenIncludeWithIncludedValueThenTrue() {
-    Interval interval = this.intervalBuilder.open(left.getEquals()).open(right.getEquals()).build();
-    assertFalse(interval.include(left.getLess()));
-    assertFalse(interval.include(left.getEquals()));
-    assertTrue(interval.include(left.getGreater()));
-    assertTrue(interval.include(right.getLess()));
-    assertFalse(interval.include(right.getEquals()));
-    assertFalse(interval.include(right.getGreater()));
-  }
+    @Test
+    public void givenIntervaOpenOpenlwhenIncludeWithIncludedValueThenTrue() {
+        Interval interval = this.intervalBuilder.open(left.getEquals()).open(right.getEquals()).build();
+        assertFalse(interval.include(left.getLess()));
+        assertFalse(interval.include(left.getEquals()));
+        assertTrue(interval.include(left.getGreater()));
+        assertTrue(interval.include(right.getLess()));
+        assertFalse(interval.include(right.getEquals()));
+        assertFalse(interval.include(right.getGreater()));
+    }
 
-  @Test
-  public void givenIntervaOpenOpenlwhenInc3ludeWithIncludedValueThenTrue() {
-    Interval interval = this.intervalBuilder.closed(left.getEquals()).open(right.getEquals()).build();
-    assertFalse(interval.include(left.getLess()));
-    assertTrue(interval.include(left.getEquals()));
-    assertTrue(interval.include(left.getGreater()));
+    @Test
+    public void givenIntervaOpenOpenlwhenInc3ludeWithIncludedValueThenTrue() {
+        Interval interval = this.intervalBuilder.closed(left.getEquals()).open(right.getEquals()).build();
+        assertFalse(interval.include(left.getLess()));
+        assertTrue(interval.include(left.getEquals()));
+        assertTrue(interval.include(left.getGreater()));
 
-    assertTrue(interval.include(right.getLess()));
-    assertFalse(interval.include(right.getEquals()));
-    assertFalse(interval.include(right.getGreater()));
-  }
+        assertTrue(interval.include(right.getLess()));
+        assertFalse(interval.include(right.getEquals()));
+        assertFalse(interval.include(right.getGreater()));
+    }
 
-  @Test
-  public void givenIntervaOpenOpenlwhenIncludeWit3hIncludedValueThenTrue() {
-    Interval interval = this.intervalBuilder.open(left.getEquals()).closed(right.getEquals()).build();
-    assertFalse(interval.include(left.getLess()));
-    assertFalse(interval.include(left.getEquals()));
-    assertTrue(interval.include(left.getGreater()));
+    @Test
+    public void givenIntervaOpenOpenlwhenIncludeWit3hIncludedValueThenTrue() {
+        Interval interval = this.intervalBuilder.open(left.getEquals()).closed(right.getEquals()).build();
+        assertFalse(interval.include(left.getLess()));
+        assertFalse(interval.include(left.getEquals()));
+        assertTrue(interval.include(left.getGreater()));
 
-    assertTrue(interval.include(right.getLess()));
-    assertTrue(interval.include(right.getEquals()));
-    assertFalse(interval.include(right.getGreater()));
-  }
+        assertTrue(interval.include(right.getLess()));
+        assertTrue(interval.include(right.getEquals()));
+        assertFalse(interval.include(right.getGreater()));
+    }
 
-  @Test
-  public void givenIntervaOpenOpenlwhenIncludeWithInclude5dValueThenTrue() {
-    Interval interval = this.intervalBuilder.closed(left.getEquals()).closed(right.getEquals()).build();
-    assertFalse(interval.include(left.getLess()));
-    assertTrue(interval.include(left.getEquals()));
-    assertTrue(interval.include(left.getGreater()));
+    @Test
+    public void givenIntervaOpenOpenlwhenIncludeWithInclude5dValueThenTrue() {
+        Interval interval = this.intervalBuilder.closed(left.getEquals()).closed(right.getEquals()).build();
+        assertFalse(interval.include(left.getLess()));
+        assertTrue(interval.include(left.getEquals()));
+        assertTrue(interval.include(left.getGreater()));
 
-    assertTrue(interval.include(right.getLess()));
-    assertTrue(interval.include(right.getEquals()));
-    assertFalse(interval.include(right.getGreater()));
-  }
+        assertTrue(interval.include(right.getLess()));
+        assertTrue(interval.include(right.getEquals()));
+        assertFalse(interval.include(right.getGreater()));
+    }
 
-  @Test
-  public void intersectsFalse() {
-    Interval one = new IntervalBuilder().open(left.getEquals()).open(left.getGreater()).build();
-    Interval two = new IntervalBuilder().open(right.getEquals()).open(right.getGreater()).build();
-    assertFalse(one.intersects(two));
-  }
+    @Test
+    public void intersectsFalse() {
+        Interval one = new IntervalBuilder().open(left.getEquals()).open(left.getGreater()).build();
+        Interval two = new IntervalBuilder().open(right.getEquals()).open(right.getGreater()).build();
+        assertFalse(one.intersects(two));
+        two = new IntervalBuilder().open(left.getLess()).open(left.getEquals()).build();
+        assertFalse(one.intersects(two));
+    }
 
-  @Test
-  public void intersectsTrue() {
-    Interval one = new IntervalBuilder().open(left.getEquals()).open(left.getGreater()).build();
-    Interval two = new IntervalBuilder().open(left.getEquals()).open(right.getGreater()).build();
-    assertTrue(one.intersects(two));
-    one = new IntervalBuilder().open(left.getLess()).open(left.getGreater()).build();
-    two = new IntervalBuilder().open(left.getEquals()).open(right.getEquals()).build();
-    assertTrue(one.intersects(two));
-    one = new IntervalBuilder().open(left.getLess()).open(left.getGreater()).build();
-    two = new IntervalBuilder().open(left.getEquals()).open(right.getGreater()).build();
-    assertTrue(one.intersects(two));
-  }
+    @Test
+    public void intersectsTrue() {
+        Interval one = new IntervalBuilder().open(left.getEquals()).open(left.getGreater()).build();
+        Interval two = new IntervalBuilder().open(left.getEquals()).open(right.getGreater()).build();
+        assertTrue(one.intersects(two));
+        one = new IntervalBuilder().open(left.getLess()).open(left.getGreater()).build();
+        two = new IntervalBuilder().open(left.getEquals()).open(right.getEquals()).build();
+        assertTrue(one.intersects(two));
+        one = new IntervalBuilder().open(left.getLess()).open(left.getGreater()).build();
+        two = new IntervalBuilder().open(left.getEquals()).open(right.getGreater()).build();
+        assertTrue(one.intersects(two));
+    }
 }
